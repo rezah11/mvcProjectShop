2d7a6e17e63790b396e76d10113f7f7ea1a2e25f
reza <reza_h11@yahoo.com>
2022-03-06 14:07:58 +0330

    Update footer.php

--- view/index/footer.php
+++ view/index/footer.php
@@ -5,5 +5,5 @@
 <script src="public/javaScript/jquery.simple.timer.js" type="text/javascript"></script>
 <script src="public/javaScript/bootstrap.js" type="text/javascript"></script>
 <script src="public/javaScript/owl.carousel.min.js" type="text/javascript"></script>
-<script src="public/javaScript/js.js" type="text/javascript"></script>
+<!--<script src="public/javaScript/js.js" type="text/javascript"></script>-->
 </html>
